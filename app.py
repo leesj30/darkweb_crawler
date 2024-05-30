e32a1612cbbddbaba9d1f8faf655a492737b9a7f
쟈에요 <105041123+namu1029@users.noreply.github.com>
2024-05-30 16:55:41 +0900

    프론트 작업

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, jsonify
+from postdb import search_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def results():
+    tgtword = request.form.get('input')
+    results = search_data(tgtword)
+    return render_template("results.html", tgtword=tgtword, results=results)
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
---
+++ postdb.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+import json
+import os
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.postdb
+collection = db.postlist
+
+def insert_json_data(json_file):
+    with open(json_file, 'r', encoding='UTF8') as file:
+        data = json.load(file)
+        if isinstance(data, dict):
+            collection.insert_one(data)
+        elif isinstance(data, list):
+            collection.insert_many(data)
+        else:
+            print("Data format not supported for insertion.")
+
+def process_all_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.json'):
+                file_path = os.path.join(root, filename)
+                insert_json_data(file_path)
+
+def search_data(keyword):
+    query = {
+        "$or": [
+            {"title": {"$regex": keyword, "$options": "i"}},
+            {"text": {"$regex": keyword, "$options": "i"}}
+        ]
+    }
+    results = collection.find(query)
+    return list(results)
+
+# directory = 'C:\\Users\\zia20\\Documents\\GitHub\\darkweb_crawler\\posts\\3am'
+# process_all_files(directory)
+
+# transfer_network_documents()
+
+search_results = search_data('mysql')
+print(search_results)

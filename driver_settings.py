ff3a20fae20fa6ebd94f6acb6cbd71729a98782e
leesj30 <127267408+leesj30@users.noreply.github.com>
2024-05-30 11:10:43 +0900

    Update driver_settings.py
    Signed-off-by: leesj30 <127267408+leesj30@users.noreply.github.com>

--- driver_settings.py
+++ driver_settings.py
@@ -5,12 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def driver_settings():
     chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--ignore-ssl-errors")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 

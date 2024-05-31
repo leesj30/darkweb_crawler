f5746a17644f257f9766650573bc7e18561b273f
leesj30 <127267408+leesj30@users.noreply.github.com>
2024-05-31 16:55:12 +0900

    Update crawler.py
    Signed-off-by: leesj30 <127267408+leesj30@users.noreply.github.com>

--- crawler.py
+++ crawler.py
@@ -7,6 +7,8 @@ from urllib.parse import urljoin
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import re
+from datetime import datetime
 from discord import send_to_discord
 from driver_settings import settings
 
@@ -197,9 +199,75 @@ def crawl_blackbasta(site_url, site_name):
             save_post_data(save_dir, post_data, hash_value)
     
     driver.quit()
+
+def crawl_RansomEXX(site_url, site_name):
+    proxies = {
+    'http': 'socks5h://127.0.0.1:9050',
+    'https': 'socks5h://127.0.0.1:9050'
+    }
+    save_dir = create_save_dir('json', site_name)
+    existing_hashes = get_existing_post_hashes(save_dir)
     
+    try:
+        # 요청 보내기
+        response = requests.get(site_url, proxies=proxies, timeout=30)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # 모든 포스트 찾기
+        posts = soup.find_all('article')
+
+        # 포스트 정보 추출 및 저장
+        for post in posts:
+            try:
+                title_tag = post.find('h2', class_='entry-title').find('a')
+                title = title_tag.get('href') if title_tag else 'No Title'
+                title = title.split('/')[-2]  # 타이틀에서 파일명 부분만 추출
+            
+                datetime_tag = post.find('time', class_='entry-date published')
+                datetime_str = datetime_tag.get('datetime') if datetime_tag else None
+                if datetime_str:
+                    datetime_str = datetime_str.replace('T', ' ')
+                datetime_obj = datetime.fromisoformat(datetime_str) if datetime_str else 'No Date'
+
+                entry_summary_tag = post.find('div', class_='entry-summary')
+                entry_summary = entry_summary_tag.text.strip() if entry_summary_tag else 'No Summary'
+
+                # Leaked data size 추출
+                leaked_data_size_match = re.search(r'Leaked data size: ([\d.]+[A-Z]+)', entry_summary)
+                leaked_data_size = leaked_data_size_match.group(1) if leaked_data_size_match else 'No Data Size'
+
+                # entry_summary에서 Leaked data size 부분 제거
+                entry_summary = re.sub(r'\nLeaked data size: [\d.]+[A-Z]+.', '', entry_summary)
+
+                country_tag_tag = post.find('a', rel='tag')
+                country_tag = country_tag_tag.get('href') if country_tag_tag else 'No Country'
+
+                
+                # JSON 데이터 생성
+                data = {
+                    'title': title,
+                    'datetime': datetime_obj.isoformat() if isinstance(datetime_obj, datetime) else datetime_str,
+                    'entry_summary': entry_summary,
+                    'leaked_data_size': leaked_data_size,
+                    'operator': 'RansomEXX'
+                }
+                
+                hash_value = hash_data(title, entry_summary)
+
+                if hash_value not in existing_hashes:
+                    save_post_data(save_dir, data, hash_value)
+                    send_to_discord('RansomEXX', 'detailed_data', data)
+            except Exception as e:
+                print(f"Error processing post: {e}")
+
+        print("Crawling and saving complete.")
+    except requests.RequestException as e:
+        print(f"Error fetching the page: {e}")
+
 
 
 #crawl_blacksuite('http://weg7sdx54bevnvulapqu6bpzwztryeflq3s23tegbmnhkbpqz637f2yd.onion/', 'blacksuite')
 #crawl_bianlianl('http://bianlianlbc5an4kgnay3opdemgcryg2kpfcbgczopmm3dnbz3uaunad.onion', 'bianlianl')
-crawl_3am('http://threeamkelxicjsaf2czjyz2lc4q3ngqkxhhlexyfcp2o6raw4rphyad.onion','3am')
+#crawl_3am('http://threeamkelxicjsaf2czjyz2lc4q3ngqkxhhlexyfcp2o6raw4rphyad.onion','3am')
+#crawl_RansomEXX('http://rnsm777cdsjrsdlbs4v5qoeppu3px6sb2igmh53jzrx7ipcrbjz5b2ad.onion/', 'RansomEXX')
